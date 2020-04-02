8809b453f9f6c5920c5c3a040bc3b738d3fd2342
Carlos Zamora Sanz <32102276+Charlo270398@users.noreply.github.com>
2020-04-02 15:15:43 +0200

    Funciones AES

--- ClienteProyectoES/src/proyectoes/Seguridad.java
+++ ClienteProyectoES/src/proyectoes/Seguridad.java
@@ -6,9 +6,16 @@
 
 package proyectoes;
 
+import AES.AES;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger; 
 import java.security.MessageDigest; 
 import java.security.NoSuchAlgorithmException; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 
 /**
  *
@@ -32,5 +39,43 @@ public class Seguridad {
 	    e.printStackTrace();
 	}
 	return toReturn;
+    }
+    
+    public static File cifrarFicheroAES(File ficheroSinCifrar, String nombreFichero, String key){
+        File ficheroCifrado = new File(nombreFichero);
+        AES    en      = new AES(key);
+        try {
+            /*
+            * write out encrypted file
+            */
+            en.WriteEncryptedFile(ficheroSinCifrar,ficheroCifrado);
+            return ficheroCifrado;
+        } catch (IOException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalBlockSizeException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BadPaddingException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public static File descifrarFicheroAES(File ficheroCifrado, String nombreFicheroDescifrado, String key){
+        File ficheroDescifrado = new File(nombreFicheroDescifrado);
+        AES    en      = new AES(key);
+        try {
+            /*
+            * write out encrypted file
+            */
+            en.ReadEncryptedFile(ficheroCifrado,ficheroDescifrado);
+            return ficheroDescifrado;
+        } catch (IOException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalBlockSizeException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (BadPaddingException ex) {
+            Logger.getLogger(Seguridad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
